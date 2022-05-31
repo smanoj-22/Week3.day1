2aa948c4951cff6cfe7f5ad586830f4182ccc594
smanoj-22 <103946797+smanoj-22@users.noreply.github.com>
2022-05-31 21:56:44 +0530

    Add files via upload

---
+++ BankInfo.java
@@ -0,0 +1,17 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public void saving() 
+	{
+		System.out.println("Saving Account Creation");
+	}
+	public void fixed() 
+	{
+		System.out.println("Fixed Amount");
+	}
+	public void deposit()
+	{
+		System.out.println("1000000");
+	}
+}
